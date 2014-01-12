6129dd1358f6c13e011255508baa6d7fb682b366
Julian Ceipek <julian.ceipek@gmail.com>
2014-01-12 18:18:31 -0500

    Basic control code

---
+++ Assets/Scripts/MoveToTarget.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToTarget : MonoBehaviour
+{
+
+	public float m_movementSpeed;
+	private Vector2 m_target;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		transform.position += ((Vector3)m_target - transform.position) * m_movementSpeed * Time.fixedDeltaTime;
+	}
+	
+	public void SetTarget (Vector2 target)
+	{
+		m_target = target;
+	}
+}
---
+++ Assets/Scripts/TargetController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(MoveToTarget))]
+public class TargetController : MonoBehaviour
+{
+
+	private MoveToTarget m_moveToTarget;
+	private float m_cameraOffset;
+
+	void OnEnable ()
+	{
+		m_moveToTarget = GetComponent<MoveToTarget> ();
+		m_cameraOffset = transform.position.z - Camera.main.transform.position.z;
+	}
+
+	
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Click")) {
+
+
+			var pos = Input.mousePosition;
+			pos.z = m_cameraOffset;
+			/*transform.position = Camera.main.ScreenToWorldPoint (pos);*/
+			m_moveToTarget.SetTarget (Camera.main.ScreenToWorldPoint (pos));
+
+		}
+	}
+}
